5f8225ae65c26c83e0fc51b88ebbead5b02d28b9
JodhanG <j.ghangheri@gmail.com>
2022-12-06 16:56:29 -0800

    - Added Typescript support - Base UI added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import './styles.css';
+import React from 'react';
 
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/index.tsx
@@ -0,0 +1,77 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+
+const Home = () => {
+  
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  const onUserChangedText = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  return (
+    <div className="root">
+      <Head>
+        <title>GPT-3 Writer</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>AI Copywriter</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Input the product and what it does, we'll do the rest</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText} />
+          <div className="prompt-buttons">
+            <a className={isGenerating ? 'generate-button loading' : 'generate-button'} onClick={callGenerateEndpoint}>
+              <div className="generate">
+              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default Home;
