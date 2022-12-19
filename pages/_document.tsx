4450a4d83c13fc0a09a614884a5f55160f0b039a
JodhanG <j.ghangheri@gmail.com>
2022-12-19 11:41:43 -0800

    - Code Cleanup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,13 +5,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
-        <meta
-          property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
-        />
-        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta property="og:title" content="GPT-3 Copywriter" key="title"/>
+        <meta property="og:description" content="AI Advertising" key="description"/>
       </Head>
       <body>
         <Main />
